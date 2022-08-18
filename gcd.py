134cea78bd72d8ecbac4d4aafad84f6a0f3d7ff2
NebulaAnish <50112251+NebulaAnish@users.noreply.github.com>
2022-08-18 18:38:34 +0545

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,13 @@
+
+def gcd(dividend, divisor):
+    if (dividend % divisor) == 0:
+        return divisor
+    return gcd(divisor, (dividend % divisor))
+
+
+def lcm(a, b):
+    return int(a*b/gcd(a, b))
+
+
+print(gcd(4, 12))
+print(lcm(4, 12))
